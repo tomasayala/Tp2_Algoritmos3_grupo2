9aa15003a6883101b625dbbd16b16394191c92a6
Tomas Ayala <tomasayala5c@gmail.com>
2020-12-17 16:24:14 -0300

    Implementacion del BloqueMovimientoDerecha

---
+++ tp2_entrega1/src/bloque/bloqueMovimiento/BloqueMovimientoDerecha.java
@@ -0,0 +1,10 @@
+package bloque.bloqueMovimiento;
+
+import direccion.DireccionDerecha;
+
+public class BloqueMovimientoDerecha extends BloqueMoviemiento{
+
+    public BloqueMovimientoDerecha(){
+        this.direccion = new DireccionDerecha();
+    }
+}
