b1b73233ce75d127f615bb21a8645f1aa95aa96a
Tomas Ayala <tomasayala5c@gmail.com>
2020-12-17 16:46:41 -0300

    Implementacion del test de BloqueMoviemientoDerecha

---
+++ tp2_entrega1/src/bloque/bloqueMovimiento/BloqueMovimientoDerechaTestMueveElPersonajeALaDerecha.java
@@ -0,0 +1,17 @@
+package bloque.bloqueMovimiento;
+
+import junit.framework.TestCase;
+import personaje.Personaje;
+
+public class BloqueMovimientoDerechaTestMueveElPersonajeALaDerecha extends TestCase {
+
+    public void testEjecutarInstruccionSobrePersonaje(){
+        Personaje personaje = new Personaje();
+        BloqueMovimientoDerecha bloque = new BloqueMovimientoDerecha();
+        int x_viejo = personaje.getCoordenada().getX();
+
+        bloque.ejecutarInstruccionSobrePersonaje(personaje);
+
+        assertEquals( x_viejo +1, personaje.getCoordenada().getX());
+    }
+}
