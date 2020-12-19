91f213d1a3ecfe29c6ac60f706c73407d377f86b
Tomas Ayala <tomasayala5c@gmail.com>
2020-12-19 16:24:55 -0300

    Posible implementacion de un patron State para el lapiz, se redifiniria al lapiz como una clase en concreto

--- tp2_entrega1/src/lapiz/Lapiz.java
+++ tp2_entrega1/src/lapiz/Lapiz.java
@@ -1,5 +1,30 @@
 package lapiz;//package com.company;
 
+
+// Posiblemente pensar patron State para distintos tipos de lapiz, lapiz rojo, azul
+/*
+    La clase abstracta seria estado y la clase lapiz es concreta. Lo que permite esto es mantener la informacion y cambiar
+    el comportamiento con el atributo state
+
+
+ */
+
+/*
+      Posible implementacion del patron State para un lapiz
+
+      public class Lapiz{
+      EstadoLapiz estado;
+      ColorLapiz color;
+
+        public Lapiz(){
+        this.estado = new LapizLevantado();
+        this.color = new ColorLapizRojo;
+        }
+        public void setEstado( EstadoLapiz estadoNuevo){
+            estado = estadoNuevo;
+        }
+      }
+ */
 public abstract class Lapiz{
     public abstract int dibujarLinea();
 }
