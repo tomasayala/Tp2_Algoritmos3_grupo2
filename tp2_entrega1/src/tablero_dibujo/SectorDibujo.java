4e5d0904924bbc59723294fcc5177c285e2d5aa6
Tomas Ayala <tomasayala5c@gmail.com>
2021-02-07 18:06:41 -0300

    Principios de SectorDibujo, posible Singleton

--- tp2_entrega1/src/tablero_dibujo/SectorDibujo.java
+++ tp2_entrega1/src/tablero_dibujo/SectorDibujo.java
@@ -1,5 +1,16 @@
 package tablero_dibujo;
 
+import coordenadas.Coordenada;
+
+import java.awt.*;
+
+// Tal vez tenga que ser un Singleton
+
 public class SectorDibujo {
     // Guardar las lineas que se dibujan en una lista o un Array?
+    private List coleccionLineas;
+    private Coordenada personaje;
+
+
+
 }
